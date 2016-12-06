7657054df55a73d26c9c0a5165d41b134463cfd5
Michael Terry <mterry@sancsoft.com>
2016-12-05 23:49:05 -0500

    base packages and app structure

---
+++ PingTrigger.Service/PingTriggerService.cs
@@ -0,0 +1,80 @@
+﻿/**************************************************************************
+ * Filename: PingTriggerService.cs
+ * Project:  PingTrigger.exe
+ * 
+ * Description:
+ * Main service class.  Runs on a periodic timer to kick the actions of 
+ * the service. 
+**************************************************************************/
+using System;
+using Serilog;
+using Ninject;
+
+namespace PingTrigger
+{
+    public class PingTriggerService
+    {
+        protected System.Timers.Timer _timer;
+        private bool processLock;
+        protected int sendBatchSize;
+
+        /// <summary>
+        /// Constructor for the service class. creates the service timer and ties it to the processing method
+        /// </summary>
+        public PingTriggerService()
+        {
+            Log.Information("Construction of PingTriggerService");
+        }
+
+        /// <summary>
+        /// Start the service by kicking the timer
+        /// </summary>
+        public void Start()
+        {
+            Log.Warning("Starting service on {0}", System.Environment.MachineName);
+
+            int dwellTime = 5000;
+            Log.Information("Message service dwell timer {0} msec.", dwellTime);
+            _timer = new System.Timers.Timer(dwellTime) { AutoReset = true };
+            _timer.Elapsed += (sender, eventArgs) => ProcessMessages();
+            _timer.Start();
+
+        }
+
+        /// <summary>
+        /// Stop the service by killing the timer
+        /// </summary>
+        public void Stop()
+        {
+            Log.Warning("Stopping service on {0}", System.Environment.MachineName);
+            _timer.Stop();
+        }
+
+
+        /// <summary>
+        /// Process messages.  This process looks for messages to send and sends a block 
+        /// of the configured batch size.  It is called based on the service timer and
+        /// blocks re-entry
+        /// </summary>
+        public void ProcessMessages()
+        {
+            // primitively prevent re-entry into the processing
+            if (processLock)
+            {
+                Log.Debug("Re-entry into service process, still busy.");
+                return;
+            }
+            Log.Verbose("BEGIN: ProcessMessages");
+            processLock = true;
+            try
+            {
+            }
+            catch (Exception e)
+            {
+                Log.Error("Unhandled exception in subtxtr service processing {0}", e);
+            }
+            processLock = false;
+            Log.Verbose("END: ProcessMessages");
+        }
+    }
+}
--- PingTrigger.Service/Program.cs
+++ PingTrigger.Service/Program.cs
@@ -1,8 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿/**************************************************************************
+ * Filename: Program.cs
+ * Project: PingTrigger.exe
+ * 
+ * Description:
+ * Entry point for the PingTrigger application.  Configures logging 
+ * and launches the service or command line application using Topshelf
+**************************************************************************/
+using System.IO;
+using System.Reflection;
+using System.Configuration;
+using Serilog;
+using Topshelf;
+using Ninject;
+using System.Net;
+
 
 namespace PingTrigger
 {
@@ -10,6 +21,35 @@ namespace PingTrigger
     {
         static void Main(string[] args)
         {
+            // configure the logging system
+            Log.Logger = new LoggerConfiguration().CreateLogger();
+            Serilog.Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Verbose()
+                .Enrich.WithProperty("appname", Assembly.GetEntryAssembly().GetName().Name)
+                .WriteTo.ColoredConsole(Serilog.Events.LogEventLevel.Verbose)
+                .WriteTo.RollingFile(Path.Combine(".", "serilog-{Date}.txt"), Serilog.Events.LogEventLevel.Debug)
+                .CreateLogger();
+
+            // log the program launch and version information
+            AssemblyName assemblyName = Assembly.GetEntryAssembly().GetName();
+            Log.Information(assemblyName.Name + " Version " + assemblyName.Version.ToString());
+            Log.Information("Copyright (c) 2016 - Sanctuary Software Studio, Inc.");
+
+            HostFactory.Run(x =>
+            {
+                x.Service<PingTriggerService>(s =>
+                {
+                    s.ConstructUsing(name => new PingTriggerService());
+                    s.WhenStarted(tc => tc.Start());
+                    s.WhenStopped(tc => tc.Stop());
+                });
+                x.RunAsLocalSystem();
+
+                x.SetDescription("PingTrigger Service");
+                x.SetDisplayName("PingTrigger");
+                x.SetServiceName("PingTrigger");
+            });
+
         }
     }
 }
